423a2b8d50506fde020ca9ef10f922404e880be6
95027 <kumarchembeti26@gmail.com>
2024-11-17 13:13:32 +0530

    added auth pages

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -1,10 +1,68 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type LoginDetails = {
+  email: string;
+  password: string;
+};
 
 const Login = () => {
-  return (
-    <div className="max-w-6xl mx-auto">
-      login
-    </div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginDetails>();
 
-export default Login
+  const onSubmit: SubmitHandler<LoginDetails> = (data) => console.log(data);
+
+  return (
+    <div className="max-w-6xl mx-auto text-primary">
+      <div className="card shadow-md mx-auto w-full max-w-screen-sm p-5 rounded-md">
+        <div className="flex items-center justify-center mb-3">
+          <h2 className="text-primary font-bold text-2xl">Login Form</h2>
+        </div>
+        <form className="" onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-3">
+            <label htmlFor="" className="text-pretty text-gray-500 font-bold">
+              Email:
+            </label>
+            <input
+              type="email"
+              className="w-full p-1 mt-1"
+              placeholder="Enter your email"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <span className="text-red-500 font-light">Email is required</span>
+            )}
+          </div>
+          <div className="mb-3">
+            <label htmlFor="" className="text-pretty text-gray-500 font-bold">
+              Password:
+            </label>
+            <input
+              type="password"
+              className="w-full p-1 mt-1"
+              placeholder="Enter your password"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <span className="text-red-500 font-light">
+                password is required
+              </span>
+            )}
+          </div>
+          <div className="flex items-center justify-end">
+            <button
+              type="submit"
+              className="btn bg-blue-600 rounded-md px-3 py-1 text-white"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/auth/Register.tsx
+++ src/auth/Register.tsx
@@ -1,11 +1,100 @@
+import { useForm, SubmitHandler } from "react-hook-form";
 
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+  confirm_password: string;
+};
 
 const Register = () => {
-  return (
-    <div className="max-w-6xl mx-auto">
-      register
-    </div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
 
-export default Register
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  return (
+    <div className="max-w-6xl mx-auto text-primary">
+      <div className="card shadow-md mx-auto w-full max-w-screen-sm p-5 rounded-md">
+        <div className="flex items-center justify-center mb-3">
+          <h2 className="text-primary font-bold text-2xl">Registration Form</h2>
+        </div>
+        <form className="" onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-3">
+            <label htmlFor="" className="text-pretty text-gray-500 font-bold">
+              Name:
+            </label>
+            <input
+              type="text"
+              className="w-full p-1 mt-1"
+              placeholder="Enter your name"
+              {...register("name", { required: true })}
+            />
+            {errors.name && (
+              <span className="text-red-500 font-light">Name is required</span>
+            )}
+          </div>
+          <div className="mb-3">
+            <label htmlFor="" className="text-pretty text-gray-500 font-bold">
+              Email:
+            </label>
+            <input
+              type="email"
+              className="w-full p-1 mt-1"
+              placeholder="Enter your email"
+              {...register("email", { required: true })}
+            />
+            {errors.name && (
+              <span className="text-red-500 font-light">Email is required</span>
+            )}
+          </div>
+          <div className="mb-3">
+            <label htmlFor="" className="text-pretty text-gray-500 font-bold">
+              Password:
+            </label>
+            <input
+              type="password"
+              className="w-full p-1 mt-1"
+              placeholder="Enter your password"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <span className="text-red-500 font-light">
+                Password is required
+              </span>
+            )}
+          </div>
+          <div className="mb-3">
+            <label htmlFor="" className="text-pretty text-gray-500 font-bold">
+              Confirm Password:
+            </label>
+            <input
+              type="password"
+              className="w-full p-1 mt-1"
+              placeholder="Confirm your password"
+              {...register("confirm_password", { required: true })}
+            />
+            {errors.confirm_password && (
+              <span className="text-red-500 font-light">
+                Confirm your Password
+              </span>
+            )}
+          </div>
+          <div className="flex items-center justify-end">
+            <button
+              type="submit"
+              className="btn bg-blue-600 rounded-md px-3 py-1 text-white"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
